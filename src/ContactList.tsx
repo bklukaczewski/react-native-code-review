2a9e6c10cca55cf4be86685a06e4f505b54f80ab
Bartosz Klukaczewski <bklukaczewski@gmail.com>
2018-07-25 14:08:32 +0200

    Configure Typescript support.

---
+++ src/ContactList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from "react-native";
+
+export class ContactList extends React.Component {
+    render() {
+        return (
+            <Text>Contact list</Text>
+        )
+    }
+}
